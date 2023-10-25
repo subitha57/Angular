a59528a8dda634e1b03c8cfb152f0744a9525a71
subitha satheesh <147406447+subitha57@users.noreply.github.com>
2023-10-25 19:32:53 +0530

    Add files via upload

--- employee.component.ts
+++ employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/service/employee.service';
 
 
 @Component({
@@ -6,98 +7,86 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent {
-  employeeObj: EmployeeObj;
-  sortBy: string;
-  searchText: string;
-  employeeArr: EmployeeObj[] = [];
 
-  constructor() {
-    this.employeeObj = new EmployeeObj();
-    this.searchText = "";
-    this.sortBy = "";
+
+export class EmployeeComponent implements OnInit {
+  employeeArray: any[] = [];
+  employeeObj: any;
+
+  constructor(private empsrv: EmployeeService) {
+    this.resetObj();
   }
-  ngOnInit() {
-    this.getAllUsers();
+  resetObj() {
+    this.employeeObj = {
+      "empId": 0,
+      "empName": "",
+      "empContactNo": "",
+      "empEmail": "",
+      "addressLine1": "",
+      "addressLine2": "",
+      "pincode": "",
+      "city": "",
+      "state": "",
+      "bankName": "",
+      "iFSC": "",
+      "accountNo": "",
+      "bankBranch": "",
+      "salary": 0
+    }
+
+  }
+
+  ngOnInit(): void {
+    debugger;
+    this.loadAllEmployee();
+  }
+  loadAllEmployee() {
+    debugger;
+    this.empsrv.getAllEmployee().subscribe((res: any) => {
+      debugger;
+      this.employeeArray = res.data;
+    })
   }
   onSave() {
-
-    this.employeeArr.push(this.employeeObj)
-    const isData = localStorage.getItem("EmpData");
-    if (isData == null) {
-      const newArr = [];
-      this.employeeObj.EmployeeId = 0;
-      newArr.push(this.employeeObj);
-      localStorage.setItem("EmpData", JSON.stringify(newArr))
-    } else {
-      const oldData = JSON.parse(isData);
-      const newId = oldData.length + 1;
-      this.employeeObj.EmployeeId = newId;
-      oldData.push(this.employeeObj);
-      localStorage.setItem('EmpData', JSON.stringify(oldData));
-    }
-    this.employeeObj = new EmployeeObj();
-    this.getAllUsers();
-  }
-  getAllUsers() {
-    const isData = localStorage.getItem("EmpData");
-    if (isData != null) {
-      const localData = JSON.parse(isData);
-      this.employeeArr = localData
-    }
-
-  }
-  onEdit(item: EmployeeObj) {
-    this.employeeObj = item;
-  }
-  onDelete(item: EmployeeObj) {
-    const isData = localStorage.getItem("EmpData");
-    if (isData != null) {
-      const localData = JSON.parse(isData);
-      for (let index = 0; index < localData.length; index++) {
-        if (localData[index].EmployeeId == item.EmployeeId) {
-          localData.splice(0, 1);
-        }
+    debugger;
+    this.empsrv.CreateEmployee(this.employeeObj).subscribe((res: any) => {
+      debugger;
+      if (res.result) {
+        this.loadAllEmployee();
+        alert(res.message);
+      } else {
+        alert(res.message);
       }
-      localStorage.setItem("EmpData", JSON.stringify(localData));
-    }
+    })
   }
-  onSearch() {
-    const isData = localStorage.getItem("EmpData");
-    if (isData != null) {
-      const localData = JSON.parse(isData);
-      if (this.sortBy == 'Name') {
-        const filteredData = localData.filter((m: EmployeeObj) => m.FirstName.startsWith(this.searchText) ||m.LastName.startsWith(this.searchText) )
-        this.employeeArr = filteredData;
+  onUpdate(){
+    this.empsrv.updateEmployee(this.employeeObj).subscribe((res: any) => {
+      debugger;
+      if (res.result) {
+        this.loadAllEmployee();
+        alert(res.message);
+      } else {
+        alert(res.message);
       }
-    }
-  }
-
-}
-export class EmployeeObj {
-  EmployeeId: number;
-  FirstName: string;
-  LastName: string;
-  Designation: string;
-  Skills: string;
-  Core: string;
-  IsCertification: string;
-  Certification: string;
-  Company: string;
-  FewDetails: string;
-
-  constructor() {
-    this.EmployeeId = 0;
-    this.FirstName = '';
-    this.LastName = '';
-    this.Designation = "";
-    this.Skills = '';
-    this.Core = '';
-    this.IsCertification = '';
-    this.Certification = '';
-    this.Company = '';
-    this.FewDetails = '';
+    })
 
   }
+  onEdit(id:number){
+    this.empsrv.getEmpById(id).subscribe((res:any)=>{
+      this.employeeObj =res.data;
+    })
 
+  }
+  onDelete(empId:number){
+    this.empsrv.deleteEmpById(empId).subscribe((res: any) => {
+      debugger;
+      if (res.result) {
+        this.loadAllEmployee();
+        alert(res.message);
+      } else {
+        alert(res.message);
+      }
+    })
+
+  }
 }
